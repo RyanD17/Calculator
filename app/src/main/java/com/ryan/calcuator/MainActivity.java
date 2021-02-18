b898d5c20ac24517564afe7b0de2939dc79f3695
Ryan <ryandajani05@gmail.com>
2021-02-17 21:15:21 -0500

    This is the first commit

---
+++ app/src/main/java/com/ryan/calcuator/MainActivity.java
@@ -0,0 +1,99 @@
+package com.ryan.calcuator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initializing all buttons, switches, ad text boxes
+        final EditText number1;
+        number1 = findViewById(R.id.number1);
+        final EditText number2;
+        number2 = findViewById(R.id.number2);
+
+        final EditText sign;
+        sign = findViewById(R.id.sign);
+
+        final TextView result;
+        result = findViewById(R.id.result);
+
+        final Button equals;
+        equals = findViewById(R.id.equals);
+
+        equals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!number1.getText().toString().equals("")&& !number2.getText().toString().equals("")){
+                    if (!sign.getText().toString().equals("+") || !sign.getText().toString().equals("-")|| !sign.getText().toString().equals("*")  || !sign.getText().toString().equals("/") ){
+                        int Int1 = Integer.parseInt(number1.getText().toString());
+                        int Int2 = Integer.parseInt(number2.getText().toString());
+                        if (sign.getText().toString().equals("+")){
+                            int sum = Int1 + Int2;
+                            result.setText(Integer.toString(sum));
+                        }
+                        if (sign.getText().toString().equals("-")){
+                            int sum = Int1 - Int2;
+                            result.setText(Integer.toString(sum));
+                        }
+                        if (sign.getText().toString().equals("*")){
+                            int sum = Int1 * Int2;
+                            result.setText(Integer.toString(sum));
+                        }
+                        if (sign.getText().toString().equals("/")){
+                            int sum = Int1 / Int2;
+                            result.setText(Integer.toString(sum));
+                        }
+                    }
+                    else{
+                        System.out.println("Error, enter an addition, subtraction, multiplication, or division sign");
+                    }
+                }
+                else{
+                    System.out.println("Error, Text field is empty ");
+                }
+            }
+        });
+
+        final Switch mySwitch = findViewById(R.id.Switch);
+
+
+        mySwitch.setOnClickListener(new View.OnClickListener() {
+
+            boolean isOn = false;
+
+            @Override
+            public void onClick(View v) {
+                if (!isOn){// The switch is on
+                    mySwitch.setText("On");//setting the text of the switch to Off
+                    mySwitch.setBackgroundColor(Color.GREEN);
+                    isOn = true;
+                    equals.setEnabled(true);
+                    number1.setEnabled(true);
+                    number2.setEnabled(true);
+                    sign.setEnabled(true);
+                }
+                else{
+                    mySwitch.setText("Off");
+                    mySwitch.setBackgroundColor(Color.RED);
+                    isOn = false;
+                    equals.setEnabled(false);
+                    number1.setEnabled(false);
+                    number2.setEnabled(false);
+                    sign.setEnabled(false);
+                }
+            }
+        });
+    }
+}
