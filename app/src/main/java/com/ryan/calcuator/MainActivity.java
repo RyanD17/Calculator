219aec479e03b00837338788e736f91bf4fa8c90
Ryan <ryandajani05@gmail.com>
2021-03-04 18:37:17 -0500

    This is the first commit
    Signed-off-by: Ryan <ryandajani05@gmail.com>

--- app/src/main/java/com/ryan/calcuator/MainActivity.java
+++ app/src/main/java/com/ryan/calcuator/MainActivity.java
@@ -33,13 +33,10 @@ public class MainActivity extends AppCompatActivity {
         final Button equals;
         equals = findViewById(R.id.equals);
 
-<<<<<<< HEAD
-=======
         final int[] counter = {0};
         TextView numCalView = findViewById(R.id.counterNumber);
 
 
->>>>>>> e5df318 (This is the first commit)
         equals.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,39 +47,34 @@ public class MainActivity extends AppCompatActivity {
                         if (sign.getText().toString().equals("+")) {
                             int sum = Int1 + Int2;
                             result.setText(Integer.toString(sum));
-<<<<<<< HEAD
-=======
+
                             numCalView.setText(Integer.toString(counter[0] += 1));
 
->>>>>>> e5df318 (This is the first commit)
                         }
                         if (sign.getText().toString().equals("-")) {
                             int sum = Int1 - Int2;
                             result.setText(Integer.toString(sum));
-<<<<<<< HEAD
-=======
+
                             numCalView.setText(Integer.toString(counter[0] += 1));
 
 
->>>>>>> e5df318 (This is the first commit)
+
                         }
                         if (sign.getText().toString().equals("*")) {
                             int sum = Int1 * Int2;
                             result.setText(Integer.toString(sum));
-<<<<<<< HEAD
-=======
+
                             numCalView.setText(Integer.toString(counter[0] += 1));
 
->>>>>>> e5df318 (This is the first commit)
+
                         }
                         if (sign.getText().toString().equals("/")) {
                             int sum = Int1 / Int2;
                             result.setText(Integer.toString(sum));
-<<<<<<< HEAD
-=======
+
                             numCalView.setText(Integer.toString(counter[0] += 1));
 
->>>>>>> e5df318 (This is the first commit)
+
                         }
                     } else {
                         System.out.println("Error, enter an addition, subtraction, multiplication, or division sign");
